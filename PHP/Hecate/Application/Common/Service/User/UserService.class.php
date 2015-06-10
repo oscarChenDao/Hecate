291e5b7f38953ef9c62a63a998b6bacc3dc1ba3b
dema <dema@juanpi.com>
2015-06-10 16:53:24 +0800

    注册用户名 或 密码

--- PHP/Hecate/Application/Common/Service/User/UserService.class.php
+++ PHP/Hecate/Application/Common/Service/User/UserService.class.php
@@ -18,9 +18,43 @@ class UserService extends BaseService{
      *                     )     [description]
      */
     public function login($username,$password){
-        
-        
+        $data=array(
+                'msg'=>'登录成功',
+                'error'=>'true',
+            );
+        if(empty($username) && empty($password)){
+            $data['error']=false;
+            $data['msg'] = '用户名或密码未填写';
+        }
+
+       $one = M('user')->where(array('username'=>$username))->find();
+       if($one){
+            if(md5($password)!=$one['password']){
+               $data['error']=false;
+                $data['msg'] = '密码不正确'; 
+            }
+            $this->loginSession($username,$one);
+       }else{
+            $data['error']=false;
+            $data['msg'] = '用户名不存在';
+       }
+
         return $data;
+    }
+
+    /**
+     *  
+     * @param  [type] $username [description]
+     * @param  [type] $info     [单条用户信息]
+     * @return [type]           [description]
+     */
+    private function loginSession($username,$info){
+        if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
+            return true;
+        }else{
+            $_SESSION['username'] = $info;
+            return true;
+        }        
     }
 	
 }
--- PHP/Hecate/Application/Index/Controller/IndexController.class.php
+++ PHP/Hecate/Application/Index/Controller/IndexController.class.php
@@ -17,18 +17,6 @@ class IndexController extends BaseController {
      * 登录
      */
     public function login(){
-       //var_dump($_SERVER);exit;
-        $username = I('post.username','','trim');
-        $password = I('post.password','','trim');
-        if(empty($username) && empty($password)){
-            $this->error('用户名或密码错误');
-        }
-        $login = service('User/user')->login($username,$password);
-        if($login['error']===false){
-            $this->error($login['msg']);    
-        }else{
-            $this->redirect($_SERVER['HTTP_REFERER']);
-        }
     	
         $this->display();
     }
@@ -37,7 +25,18 @@ class IndexController extends BaseController {
      * 登录流程
      */
     public function doLogin(){
-         
+        //var_dump($_SERVER);exit;
+        $username = I('post.username','','trim');
+        $password = I('post.password','','trim');
+        if(empty($username) && empty($password)){
+            $this->error('用户名或密码错误');
+        }
+        $login = service('User\User')->login($username,$password);
+        if($login['error']===false){
+            $this->error($login['msg']);    
+        }else{
+            $this->redirect($_SERVER['HTTP_REFERER']);
+        }
         $this->display();
     }
     
@@ -56,7 +55,6 @@ class IndexController extends BaseController {
     public function doRegister(){
         $data=array(
             'username' =>I('post.username','','trim'),
-            'email'=>I('post.email','','trim'),
             'password' => I('post.password','','trim'),
             'repassword' => I('post.repassword','','trim'),
             'create_time' => time(),
@@ -64,16 +62,18 @@ class IndexController extends BaseController {
         
         $rules = array(     
             array('username','require','用户名必须！'), //默认情况下用正则进行验证  
+            array('username','/^[0-9a-zA-Z\x{4e00}-\x{9fa5}][0-9a-zA-Z\x{4e00}-\x{9fa5}_]*$/u','用户名格式不正确',1,'regex'), 
             array('password','6,20','密码长度6-20',1,'length'), 
-            array('email','email','邮箱格式不正确'), 
             array('username','','帐号名称已经存在！',1,'unique',1), // 在新增的时候验证name字段是否唯一       
             array('repassword','password','确认密码不正确',1,'confirm'), // 验证确认密码是否和密码一致     
         );
-        $va=M('user')->validate($rules)->create($data);
+        $userModel = M('user');
+        $va=$userModel->validate($rules)->create($data);
         if($va===false){
             //var_dump(M('user')->getError());
            $this->error((M('user')->getError()));
         }
+        $userModel->password = md5($userModel->password);
         $add=M('user')->add();
         if($add){
             //TODO
