da33ab782193d65ee10b52fcb6a4178e96af0c98
dema <dema@juanpi.com>
2015-06-10 11:59:26 +0800

    注册

--- PHP/Hecate/Application/Common/Service/User/UserService.class.php
+++ PHP/Hecate/Application/Common/Service/User/UserService.class.php
@@ -8,6 +8,19 @@ use Common\Model;
 
 class UserService extends BaseService{
     
-
+    /**
+     * [login description]
+     * @param  [type] $username [description]
+     * @param  [type] $password [description]
+     * @return [type]      $data=array(
+     *                         'msg'=>''
+     *                         'error'=>''
+     *                     )     [description]
+     */
+    public function login($username,$password){
+        
+        
+        return $data;
+    }
 	
 }
--- PHP/Hecate/Application/Index/Controller/IndexController.class.php
+++ PHP/Hecate/Application/Index/Controller/IndexController.class.php
@@ -17,6 +17,18 @@ class IndexController extends BaseController {
      * 登录
      */
     public function login(){
+       //var_dump($_SERVER);exit;
+        $username = I('post.username','','trim');
+        $password = I('post.password','','trim');
+        if(empty($username) && empty($password)){
+            $this->error('用户名或密码错误');
+        }
+        $login = service('User/user')->login($username,$password);
+        if($login['error']===false){
+            $this->error($login['msg']);    
+        }else{
+            $this->redirect($_SERVER['HTTP_REFERER']);
+        }
     	
         $this->display();
     }
@@ -42,8 +54,35 @@ class IndexController extends BaseController {
      * 注册流程
      */
     public function doRegister(){
-    
-        $this->display();
+        $data=array(
+            'username' =>I('post.username','','trim'),
+            'email'=>I('post.email','','trim'),
+            'password' => I('post.password','','trim'),
+            'repassword' => I('post.repassword','','trim'),
+            'create_time' => time(),
+        );
+        
+        $rules = array(     
+            array('username','require','用户名必须！'), //默认情况下用正则进行验证  
+            array('password','6,20','密码长度6-20',1,'length'), 
+            array('email','email','邮箱格式不正确'), 
+            array('username','','帐号名称已经存在！',1,'unique',1), // 在新增的时候验证name字段是否唯一       
+            array('repassword','password','确认密码不正确',1,'confirm'), // 验证确认密码是否和密码一致     
+        );
+        $va=M('user')->validate($rules)->create($data);
+        if($va===false){
+            //var_dump(M('user')->getError());
+           $this->error((M('user')->getError()));
+        }
+        $add=M('user')->add();
+        if($add){
+            //TODO
+            $this->success('注册成功',U('login'));    
+        }else{
+            $this->error('注册失败');  
+        }
+
+       // $this->display();
     }
 }
 
